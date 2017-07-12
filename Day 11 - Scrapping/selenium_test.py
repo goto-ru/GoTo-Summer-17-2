f0c1bbb1510770d0d88d4d12b7c3731cd9a5c132
roctbb <roctbb@gmail.com>
2017-07-12 13:18:29 +0300

    add day 11

--- Day 11 - Scrapping/selenium_test.py
+++ Day 11 - Scrapping/selenium_test.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.proxy import Proxy
+from selenium.webdriver.common.proxy import ProxyType
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+
+desired_capabilities = webdriver.DesiredCapabilities.PHANTOMJS.copy()
+driver = webdriver.PhantomJS(desired_capabilities=desired_capabilities)
+driver.get('https://ru.dotabuff.com/heroes/abaddon')
+print(driver.page_source)
